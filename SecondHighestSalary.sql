96020c471b4519652eb96c70fdf30f892dfbb5ec
Pratikshya Timalsina <31149834+pratikshya7@users.noreply.github.com>
2020-06-19 13:12:12 -0500

    Create SecondHighestSalary.sql

---
+++ SecondHighestSalary.sql
@@ -0,0 +1,23 @@
+/* ##QUESTION: 
+Write a SQL query to get the second highest salary from the Employee table.
+
++----+--------+
+| Id | Salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+For example, given the above Employee table, the query should return 200 as the second highest salary. If there is no second highest salary, then the query should return null.
+
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| 200                 |
+
+##SOLUTION: 
+SELECT Salary as SecondHighestSalary From Employee
+Where Salary < (SELECT Distinct Salary from Employee Order By Salary Desc Limit 1)
+Order By Salary Desc
+Limit 1;
+*/
