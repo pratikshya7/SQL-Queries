7365dca6f81385c4722cf4f3ff443a87799e39db
Pratikshya Timalsina <31149834+pratikshya7@users.noreply.github.com>
2020-06-19 14:26:22 -0500

    Create Nth Highest Salary.sql

---
+++ Nth Highest Salary.sql
@@ -0,0 +1,32 @@
+/*
+###Question:
+Write a SQL query to get the nth highest salary from the Employee table.
+
++----+--------+
+| Id | Salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+For example, given the above Employee table, the nth highest salary where n = 2 is 200. If there is no nth highest salary, then the query should return null.
+
++------------------------+
+| getNthHighestSalary(2) |
++------------------------+
+| 200                    |
++------------------------+
+
+###SOLUTION
+*/
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+Declare Y INT;
+SET Y = N-1;
+  RETURN (
+      # Write your MySQL query statement below.
+      SELECT Distinct Salary as NthHighestSalary From Employee
+      Order By Salary DESC
+      Limit 1 Offset Y     
+  );
+END
